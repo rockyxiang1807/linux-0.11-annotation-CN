6bed061e38ed8210c46d0e3857ffa75843890048
unknown <rocky1807@xwh.com>
2023-02-15 22:23:27 +0800

    51232

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -13,7 +13,7 @@
 #include <signal.h>
 
 #if (NR_OPEN > 32)
-#error "Currently the close-on-exec-flags are in one word, max 32 files/proc"
+#error "Currently the close-on-exec-flags are in one word, max 32 files/proc"  /* 目前关闭执行标志是一个字，最多32个文件/proc */
 #endif
 
 #define TASK_RUNNING		0
@@ -76,39 +76,40 @@ struct tss_struct {
 };
 
 struct task_struct {
-/* these are hardcoded - don't touch */
-	long state;	/* -1 unrunnable, 0 runnable, >0 stopped *///任务的运行状态
-	long counter;//任务运行时间计数（递减）（滴答数），运行时间片
-	long priority;
-	long signal;
-	struct sigaction sigaction[32];
-	long blocked;	/* bitmap of masked signals */
+/* these are hardcoded - don't touch */  /* 这些是硬编码的，不要碰 */
+	long state;	/* -1 unrunnable, 0 runnable, >0 stopped */		//任务的运行状态
+	long counter;												//任务运行时间计数（递减）（滴答数），运行时间片
+	long priority;												//运行优先数。任务开始运行时counter=priority，越大运行越长
+	long signal;												//信号。是位图，每个比特位代表一种信号，信号值=位偏移值+1
+	struct sigaction sigaction[32]; 							//信号执行属性结构，对应信号将要执行的操作和标志信息
+	long blocked;	/* bitmap of masked signals */				//进程信号屏蔽码（对应信号位图）
 /* various fields */
-	int exit_code;
-	unsigned long start_code,end_code,end_data,brk,start_stack;//代码段地址，代码长度（字节数），代码长度（字节数）加 数据长度（字节数），总长度（字节数），堆栈段地址
-	long pid,father,pgrp,session,leader;//进程标识号（进程号）,父进程号，父进程组号，会话号，会话首领
-	unsigned short uid,euid,suid;//用户标识号（用户id），有效用户id，保存的用户id
-	unsigned short gid,egid,sgid;//组标识号（组id），有效组id，保存的组id
-	long alarm;//报警定时值（滴答数）
-	long utime,stime,cutime,cstime,start_time;//用户态运行时间（滴答数），系统态运行时间（滴答数），子进程用户态运行时间，子进程系统态运行时间，进程开始运行时刻
-	unsigned short used_math;//标志：是否使用了协处理器
-/* file system info */
-	int tty;		/* -1 if no tty, so it must be signed *///进程使用tty的子设备号，-1表示没有使用
-	unsigned short umask;//文件创建属性屏蔽位
-	struct m_inode * pwd;
-	struct m_inode * root;
-	struct m_inode * executable;
-	unsigned long close_on_exec;
-	struct file * filp[NR_OPEN];
+	int exit_code;												//任务执行停止的退出码，其父进程会取
+	unsigned long start_code,end_code,end_data,brk,start_stack;	//代码段地址，代码长度（字节数），代码长度（字节数）加 数据长度（字节数），总长度（字节数），堆栈段地址
+	long pid,father,pgrp,session,leader;						//进程标识号（进程号）,父进程号，父进程组号，会话号，会话首领
+	unsigned short uid,euid,suid;								//用户标识号（用户id），有效用户id，保存的用户id
+	unsigned short gid,egid,sgid;								//组标识号（组id），有效组id，保存的组id
+	long alarm;													//报警定时值（滴答数）
+	long utime,stime,cutime,cstime,start_time;					//用户态运行时间（滴答数），系统态运行时间（滴答数），子进程用户态运行时间，子进程系统态运行时间，进程开始运行时刻
+	unsigned short used_math;									//标志：是否使用了协处理器
+/* file system info */				/* 文件系统信息 */
+	int tty;		/* -1 if no tty, so it must be signed */	//进程使用tty的子设备号，-1表示没有使用
+	unsigned short umask;										//文件创建属性屏蔽位
+	struct m_inode * pwd;										//当前工作目录i节点结构
+	struct m_inode * root;										//根目录i节点结构
+	struct m_inode * executable;								//执行文件i节点结构
+	unsigned long close_on_exec;								//执行时关闭文件句柄位图标志（参见include/fcntl.h）
+	struct file * filp[NR_OPEN];								//文件结构指针表，最多32项，表项号即是文件描述符的值
 /* ldt for this task 0 - zero 1 - cs 2 - ds&ss */
-	struct desc_struct ldt[3];
+	struct desc_struct ldt[3];									//任务局部描述符表。0-空，1-代码段cs，2-数据和堆栈段ds&ss
 /* tss for this task */
-	struct tss_struct tss;
+	struct tss_struct tss;										//进程的任务状态段信息结构
 };
 
 /*
  *  INIT_TASK is used to set up the first task table, touch at
  * your own risk!. Base=0, limit=0x9ffff (=640kB)
+ * INIT TASK用于建立第一个任务表，操作风险自负
  */
 #define INIT_TASK \
 /* state etc */	{ 0,15,15, \
