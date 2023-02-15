64f0004e74a3d09acb2cbe5fe8b54a34ed4864ab
rocky <miataxiang@163.com>
2023-02-15 18:57:59 +0800

    cc

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -86,15 +86,15 @@ struct task_struct {
 /* various fields */
 	int exit_code;
 	unsigned long start_code,end_code,end_data,brk,start_stack;//代码段地址，代码长度（字节数），代码长度（字节数）加 数据长度（字节数），总长度（字节数），堆栈段地址
-	long pid,father,pgrp,session,leader;//进程标识号（进程号）
-	unsigned short uid,euid,suid;
-	unsigned short gid,egid,sgid;
-	long alarm;
-	long utime,stime,cutime,cstime,start_time;
-	unsigned short used_math;
+	long pid,father,pgrp,session,leader;//进程标识号（进程号）,父进程号，父进程组号，会话号，会话首领
+	unsigned short uid,euid,suid;//用户标识号（用户id），有效用户id，保存的用户id
+	unsigned short gid,egid,sgid;//组标识号（组id），有效组id，保存的组id
+	long alarm;//报警定时值（滴答数）
+	long utime,stime,cutime,cstime,start_time;//用户态运行时间（滴答数），系统态运行时间（滴答数），子进程用户态运行时间，子进程系统态运行时间，进程开始运行时刻
+	unsigned short used_math;//标志：是否使用了协处理器
 /* file system info */
-	int tty;		/* -1 if no tty, so it must be signed */
-	unsigned short umask;
+	int tty;		/* -1 if no tty, so it must be signed *///进程使用tty的子设备号，-1表示没有使用
+	unsigned short umask;//文件创建属性屏蔽位
 	struct m_inode * pwd;
 	struct m_inode * root;
 	struct m_inode * executable;
